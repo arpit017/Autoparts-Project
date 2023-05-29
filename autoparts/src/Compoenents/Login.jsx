f049051f33f1357b39a84d4a2ce07641f0da449d
akumar451998 <akumar451996@gmail.com>
2023-05-29 11:51:15 +0530

    basic

---
+++ autoparts/src/Compoenents/Login.jsx
@@ -0,0 +1,92 @@
+import React, { useState ,useEffect} from 'react';
+//import { useNavigate } from 'react-router-dom';
+import { auth} from '../Firebase';
+import {db} from '../Firebase'
+import { FcGoogle } from 'react-icons/fc';
+import {
+  Box,
+  Heading,
+  Input,
+  Button,
+  FormControl,
+  Text,
+  FormLabel,
+  Center
+} from '@chakra-ui/react';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+export default function Login() {
+  //const navigate = useNavigate();
+  const [user, setUser] = useState({
+    email: "",
+    password: ""
+  });
+
+
+ 
+  const handleSignIn = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, user.email, user.password);
+     // navigate('/');
+    } catch (error) {
+      alert("Error");
+    }
+  };
+
+
+  return (
+    <Box
+      bg="gray.100"
+      minH={["60vh", "70vh"]}
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      px={["20px", "0"]}
+    >
+      <Box
+        bg="white"
+        borderRadius="md"
+        p={8}
+        boxShadow="md"
+        maxW={["sm", "md"]}
+        w="full"
+      >
+        <Heading as="h2" size="lg" textAlign="center" mb={6}>
+          Login
+        </Heading>
+        <form onSubmit={handleSignIn}>
+          <FormControl id="lemail">
+            <FormLabel>Email</FormLabel>
+            <Input onChange={(e) => {
+              setUser({ ...user, email: e.target.value })
+            }} type="email" placeholder="Enter your email" mb={4} />
+          </FormControl>
+          <FormControl id="password">
+            <FormLabel>Password</FormLabel>
+            <Input onChange={(e) => {
+              setUser({ ...user, password: e.target.value })
+            }} type="password" placeholder="Enter your password" mb={6} />
+          </FormControl>
+          <Button type='submit' colorScheme="blue" size="lg" width="full">
+            Sign In
+          </Button>
+          <Center>
+            <Button
+              mt={4}
+              leftIcon={<FcGoogle />}
+              colorScheme="red"
+              variant="outline"
+            >
+              Login with Google
+            </Button>
+          </Center>
+          <Text _hover={{ cursor: "pointer" }} c="#FFD5BF" fontWeight={"600"} mt={2}>Don't have an account?<span onClick={() => {
+            //navigate('/signup');
+          }} style={{ color: "#7766DB", fontWeight: "700" }}> Sign Up</span></Text>
+        </form>
+      </Box>
+    </Box>
+  );
+}
